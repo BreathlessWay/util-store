d3fee0fdaf8397bae9ace9c8a83aeccd497bf593
BreathlessWay <731005087@qq.com>
2021-07-16 14:51:43 +0800

    style: update

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,16 @@
-import * as proxyClickEvent from "EventSource/proxyClickEvent";
+import { proxyClickEvent } from "EventSource/proxyClickEvent";
 import { proxyRouteEvent } from "EventSource/proxyRouteEvent";
 import CookieHandler from "utils/CookieHandler";
 import EventBus from "utils/EventBus";
-import { generateNormalId, generateUUID } from "utils/generateId";
-import { getDomPath } from "utils/getDomPath";
-import { getDomPathWithIndex } from "utils/getDomPathWithIndex";
-import { getTimestamp } from "utils/getTimestamp";
 import GetType from "utils/GetType";
 import ValidatorFunc from "utils/validatorFunc";
 import * as validParams from "utils/validParams";
-import { getRelativePosition } from "utils/getRelativePosition";
 
 export default {
-  ...proxyClickEvent,
+  proxyClickEvent,
   proxyRouteEvent,
   CookieHandler,
   EventBus,
-  generateUUID,
-  generateNormalId,
-  getDomPath,
-  getDomPathWithIndex,
-  getRelativePosition,
-  getTimestamp,
   GetType,
   ValidatorFunc,
   ...validParams,
